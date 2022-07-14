f42556f1bdb492c005b2594af9cd91289abe4e4f
Serhii Orlenko <sorlenk@gmail.com>
2022-07-14 09:16:07 +0200

    C04 Refactored the JS. Add setErrorMsg function.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
+// Defining the variable and function
 const form = document.getElementById("form");
 const email = document.getElementById("email");
-const errorMsg = document.getElementById("errorMsg");
 const regex = /^[a-zA-Z]+@[a-zA-Z.-]+.[a-z]{2,4}$/;
+const setErrorMsg = (parent, whatEl, msg, className) => {
+  // Defining the variable and function
+  const newElement = document.createElement(whatEl);
+  newElement.innerText = msg;
+  newElement.classList.add(className);
+  setTimeout(() => {
+    newElement.classList.add("show");
+  }, 100);
 
+  // Procede...
+  return parent.appendChild(newElement);
+};
+
+// Procede...
 form.addEventListener("submit", (e) => {
   const isValid = regex.test(email.value);
 
-  isValid ? e.submit() : e.preventDefault(), (errorMsg.className = "show");
+  if (!isValid) {
+    e.preventDefault();
+    // errorMsg.className = "show";
+    setErrorMsg(form, "small", "Oops! Please check your email", "error-msg");
+  } else {
+    e.submit();
+  }
 });
